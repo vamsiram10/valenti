f0514c2986571eef1f6f8671b1bde8a7631e6d52
Rongali Vamsi <vamsirama1622@gmail.com>
2026-02-14 22:50:07 +0530

    all done

--- src/app/components/section/thirdpage/ThirdPage.jsx
+++ src/app/components/section/thirdpage/ThirdPage.jsx
@@ -423,7 +423,7 @@ const ThirdPage = () => {
                   alt="Popup"
                   style={{
                     maxWidth: "90vw",
-                    maxHeight: "91vh",
+                    maxHeight: "90vh",
                     background: "white",
                     borderRadius: "10px",
                     boxShadow: "0 2px 16px rgba(0,0,0,0.4)",
